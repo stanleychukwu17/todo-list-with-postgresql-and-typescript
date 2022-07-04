5a4da8d67c18163b489704b91c685eee3aecfe92
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-04 19:39:37 +0100

    added a route for getting all the todos     added a route for getting just one todo     added a route for deleting and updating a todo

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -11,6 +11,36 @@ const app = express();
 app.use(express.json());
 
 
+// get all the todo
+app.get('/todo', async (req, res) => {
+    try {
+        try {
+            const newTodo = await pool.query("SELECT * FROM todo");
+            res.json({msg:'okay', 'result': newTodo.rows});
+        } catch (err) {
+            res.json({msg:'bad', 'result':err});
+        }
+    } catch (err) {
+        res.json({msg:'bad', 'result':'A try and catch error', err});
+    }
+})
+
+// get just one request
+app.get('/todo/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        try {
+            const newTodo = await pool.query("SELECT * FROM todo where id = $1", [id]);
+            res.json({msg:'okay', 'result': newTodo.rows[0]});
+        } catch (err) {
+            res.json({msg:'bad', 'result':err});
+        }
+    } catch (err) {
+        res.json({msg:'bad', 'result':'A try and catch error', err});
+    }
+})
+
 // creating/adding a new todo
 app.post('/todo', async(req, res) => {
     try {
@@ -20,12 +50,41 @@ app.post('/todo', async(req, res) => {
             const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]);
             res.json({msg:'okay', 'result': newTodo.rows[0]});
         } catch (err) {
-            console.log(err);
-            return {'msg':'bad', 'cause':err}
+            res.json({msg:'bad', 'result':'A try and catch error', err});
         }
 
     } catch (err) {
-        console.log(err)
+        res.json({msg:'bad', 'result':'A try and catch error', err});
+    }
+})
+
+// updating an item from the todoList
+app.put('/todo/:id', async (req, res) => {
+    const id = req.params.id;
+    const {description} = req.body;
+
+    pool.query("UPDATE todo SET description = $1 WHERE id = $2", [description, id], (err: any, result: any) => {
+        if (err) {
+            res.json({msg:'bad', 'result':err});
+        }
+
+        res.json({msg:'okay', 'result': 'Todo updated successfully'});
+    })
+})
+
+// deleting an item from the todoList
+app.delete('/todo/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        try {
+            const newTodo = await pool.query("SELECT * FROM todo where id = $1", [id]);
+            res.json({msg:'okay', 'result': newTodo.rows[0]});
+        } catch (err) {
+            res.json({msg:'bad', 'result':err});
+        }
+    } catch (err) {
+        res.json({msg:'bad', 'result':'A try and catch error', err});
     }
 })
 
