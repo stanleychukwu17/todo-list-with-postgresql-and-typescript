9ea6d3bc373747d7e6e0ee19472fe287edcaa458
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-18 21:08:10 +0100

    finished the frontEnd Design for adding of an item to the todo list

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,19 +1,30 @@
 import './App.scss';
+import dog from '../../images/dog.jpg'
 
 import {Input} from '../input/Input'
-import {TodoEch} from '../todo/TodoEch';
+import {TodoEch} from '../todo/TodoEch'
 
 function App() {
     return (
         <div className="AppMain">
-            <div className='AppMain__Header'><h1>A Todo List</h1></div>
+            <div className='AppMain__Header'>
+                <div>
+                    <div><h1>A Todo List</h1></div>
+                    <div className='AppPrf_name'>Stanley Chukwu</div>
+                </div>
+                <div className='AppPrf_cvr'>
+                    <div className='AppPrf_img'><img src={dog} alt="" /></div>
+                </div>
+            </div>
             <Input />
             <div className="AppMain__EchT">
                 <div className="EchT1">Description</div>
                 <div className="EchT2">Edit</div>
                 <div className="EchT3">Delete</div>
             </div>
-            <TodoEch />
+            <div className="AppShow__EchT">
+                <TodoEch />
+            </div>
         </div>
     );
 }
--- frontend/src/components/todo/TodoEch.tsx
+++ frontend/src/components/todo/TodoEch.tsx
@@ -1,6 +1,6 @@
 import './TodoEch.scss'
 import {FaPen} from 'react-icons/fa'
-import {FaTrash} from 'react-icons/fa'
+import {FaTrashRestore} from 'react-icons/fa'
 
 export const TodoEch = () => {
     return (
@@ -8,17 +8,17 @@ export const TodoEch = () => {
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
                 <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
-                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
+                <div className="EchT3"><span><FaTrashRestore size="11px" className='iconsO' /> Delete</span></div>
             </div>
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
                 <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
-                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
+                <div className="EchT3"><span><FaTrashRestore size="11px" className='iconsO' /> Delete</span></div>
             </div>
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
                 <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
-                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
+                <div className="EchT3"><span><FaTrashRestore size="11px" className='iconsO' /> Delete</span></div>
             </div>
         </div>
     )
