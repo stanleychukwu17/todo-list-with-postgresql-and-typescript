cdaccf4493977793707a9c614d9dc417f9c126a8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-18 06:54:46 +0100

    added a package - react-icons used the react-icons on the editing and deleting of todos

--- frontend/src/components/todo/TodoEch.tsx
+++ frontend/src/components/todo/TodoEch.tsx
@@ -1,22 +1,24 @@
 import './TodoEch.scss'
+import {FaPen} from 'react-icons/fa'
+import {FaTrash} from 'react-icons/fa'
 
 export const TodoEch = () => {
     return (
         <div>
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
-                <div className="EchT2">Edit</div>
-                <div className="EchT3">Delete</div>
+                <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
+                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
             </div>
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
-                <div className="EchT2">Edit</div>
-                <div className="EchT3">Delete</div>
+                <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
+                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
             </div>
             <div className="AppMain__EchT AppMain__smallText">
                 <div className="EchT1">Description</div>
-                <div className="EchT2">Edit</div>
-                <div className="EchT3">Delete</div>
+                <div className="EchT2"><span><FaPen size="11px" className='iconsO' /> Edit</span></div>
+                <div className="EchT3"><span><FaTrash size="11px" className='iconsO' /> Delete</span></div>
             </div>
         </div>
     )
